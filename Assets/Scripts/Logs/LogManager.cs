2e34b36fc7ee33091632e7ea8204abf9aa9ad068
Onekii <kian.fitzpatrick2000@gmail.com>
2020-03-29 16:04:44 +0100

    Added base for logs to be created for each type

---
+++ Assets/Scripts/Logs/LogManager.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Logs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+public static class LogManager
+{
+    internal static string LogFolderLocation = $"{AppDomain.CurrentDomain.BaseDirectory}\\Logs\\";
+
+    public static Logger GetLoggerForCurrentClass([CallerMemberName]string callingClassName = "")
+    {
+        if (callingClassName == "")
+            return null;
+
+        Logger classLogger = _loggers.Find(x => x.ClassName == callingClassName);
+        if (classLogger is null)
+        {
+            classLogger = new Logger(callingClassName);
+            _loggers.Add(classLogger);
+        }
+
+        return classLogger;
+    }
+
+    private static List<Logger> _loggers = new List<Logger>();
+}
---
+++ Assets/Scripts/Logs/Logger.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Logs
+{
+    public class Logger
+    {
+        private readonly string _callingClassName;
+
+        string fileName;
+
+        // FIXME: add these to a config file
+        public LogTypes MinLogLevel { get; set; } = LogTypes.INFO;
+        public LogTypes MaxLogLevel { get; set; } = LogTypes.VERBOSE;
+
+
+        public string ClassName { get => _callingClassName;  }
+
+        public Logger(string callingClassName)
+        {
+            this._callingClassName = callingClassName;
+
+            CreateNewLogFile();
+        }
+
+        public enum LogTypes
+        {
+            INFO,
+            DEBUG,
+            WARNING,
+            ERROR,
+            VERBOSE
+        }
+
+        private void CreateNewLogFile()
+        {
+            if (!Directory.Exists(LogManager.LogFolderLocation))
+                Directory.CreateDirectory(LogManager.LogFolderLocation);
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                var createdLog = new FileStream($"{LogManager.LogFolderLocation}{_callingClassName}_{DateTime.Now:yyyyMMdd_HHmmss}.log", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                createdLog.Close();
+
+                fileName = createdLog.Name;
+            }
+        }
+
+        private bool ShouldLog(LogTypes type) => (LogTypes)(MaxLogLevel - type) >= MinLogLevel; 
+
+        private void Log(LogTypes type, string message)
+        {
+            string logMessgae = $"[{DateTime.Now}][{type}]: {message}" + Environment.NewLine;
+
+            File.AppendAllText(fileName, logMessgae);
+        }
+
+        public void LogError(string msg, Exception ex = null)
+        {
+            if (ShouldLog(LogTypes.ERROR))
+            {
+                Log(LogTypes.ERROR, msg);
+                Log(LogTypes.ERROR, ex.Message);
+                Log(LogTypes.ERROR, ex.StackTrace);
+            }
+        }
+
+        public void LogWarning(string msg)
+        {
+            if (ShouldLog(LogTypes.WARNING))
+            {
+                Log(LogTypes.WARNING, msg);
+            }
+        }
+
+        public void LogDebug(string msg)
+        {
+            if (ShouldLog(LogTypes.DEBUG))
+            {
+                Log(LogTypes.DEBUG, msg);
+            }
+        }
+
+        public void LogVerbose(string msg)
+        {
+            if (ShouldLog(LogTypes.VERBOSE))
+            {
+                Log(LogTypes.VERBOSE, msg);
+            }
+        }
+
+        public void LogInfo(string msg)
+        {
+            if (ShouldLog(LogTypes.INFO))
+            {
+                Log(LogTypes.INFO, msg);
+            }
+        }
+    }
+}
